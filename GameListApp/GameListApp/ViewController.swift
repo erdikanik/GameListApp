c705067618dde511ec71ac9ed48dc9cec2e07516
ka-ni-ke <erdikanik@gmail.com>
2023-11-04 13:53:29 +0300

    Create project

---
+++ GameListApp/GameListApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GameListApp
+//
+//  Created by Erdi Kanık on 4.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
