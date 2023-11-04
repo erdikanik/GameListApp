1c2450322557a014036698862852b218904f6862
ka-ni-ke <erdikanik@gmail.com>
2023-11-04 22:57:00 +0300

    Add game grid cell

---
+++ GameListApp/GameListApp/Reusables/Cells/GameGridCell.swift
@@ -0,0 +1,42 @@
+//
+//  GameGridCell.swift
+//  GameListApp
+//
+//  Created by Erdi Kanık on 4.11.2023.
+//
+
+import Foundation
+import UIKit
+
+final class GameGridCell: UICollectionViewCell {
+
+    @IBOutlet weak var gameImageView: UIImageView!
+    
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var genresLabel: UILabel!
+    @IBOutlet private weak var ratingLabel: UILabel!
+
+    var name = "" {
+        didSet {
+            nameLabel.text = name
+        }
+    }
+
+    var genres = "" {
+        didSet {
+            genresLabel.text = genres
+        }
+    }
+
+    var rating = 0 {
+        didSet {
+            ratingLabel.text = "Metacritic score: " + "\(rating)"
+        }
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+        gameImageView.image = nil
+    }
+}
