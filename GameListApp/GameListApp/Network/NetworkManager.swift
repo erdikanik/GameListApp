1ad9af2545c33fc6dfb73145d64f3680096fc87e
ka-ni-ke <erdikanik@gmail.com>
2023-11-04 18:36:52 +0300

    Add perform request method to Network Manager

--- GameListApp/GameListApp/Network/NetworkManager.swift
+++ GameListApp/GameListApp/Network/NetworkManager.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+enum Result<T> {
+    case success(T)
+    case error(Error)
+}
+
+protocol NetworkManagerProtocol {
+
+    func performRequest<T: Decodable>(request: BaseRequest, completion: @escaping (Result<T>) -> Void) -> URLSessionTask?
+}
+
+final class NetworkManager: NetworkManagerProtocol {
+
+    func performRequest<T: Decodable>(request: BaseRequest, completion: @escaping (Result<T>) -> Void) -> URLSessionTask? {
+        guard let networkRequest = request.asURLRequest() else { return nil }
+
+        let task = URLSession.shared.dataTask(with: networkRequest) { (data, _, error) in
+            guard let data = data else {
+                completion(Result.error(error ?? NSError()))
+                return
+            }
+
+            let decoder = JSONDecoder()
+
+            do {
+                let responseModel = try decoder.decode(T.self, from: data)
+                completion(Result.success(responseModel))
+            } catch {
+                completion(Result.error(error))
+            }
+        }
+
+        task.resume()
+        return task
+    }
+}
