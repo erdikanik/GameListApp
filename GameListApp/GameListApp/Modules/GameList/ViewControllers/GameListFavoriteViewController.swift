9ef6a47dc996f9c96858400db7b257a9abc775e2
ka-ni-ke <erdikanik@gmail.com>
2023-11-05 17:50:47 +0300

    Add favorite page and view model

---
+++ GameListApp/GameListApp/Modules/GameList/ViewControllers/GameListFavoriteViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GameListFavoriteViewController.swift
+//  GameListApp
+//
+//  Created by Erdi Kanık on 5.11.2023.
+//
+
+import Foundation
+import UIKit
+
+final class GameListFavoriteViewController {
+    
+}
---
+++ GameListApp/GameListApp/Modules/GameList/ViewModels/GameListFavoriteViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  GameListFavoriteViewModel.swift
+//  GameListApp
+//
+//  Created by Erdi Kanık on 5.11.2023.
+//
+
+import Foundation
+
+
+
+final class GameListFavoriteViewModel {
+
+
+}
