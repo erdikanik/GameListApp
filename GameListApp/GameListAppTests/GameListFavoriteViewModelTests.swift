00610a33d6d1e7fa1e7b0932ff79ec6f073a0305
ka-ni-ke <erdikanik@gmail.com>
2023-11-05 22:30:44 +0300

    Add favorites page test

--- GameListApp/GameListAppTests/GameListFavoriteViewModelTests.swift
+++ GameListApp/GameListAppTests/GameListFavoriteViewModelTests.swift
@@ -38,12 +38,27 @@ final class GameListFavoriteViewModelTests: XCTestCase {
         mockDataSource = DataSource()
         viewModel = GameListFavoriteViewModel(dataSource: mockDataSource)
 
-        viewModel.stateChangeHandler = { [weak self] state in
+        viewModel.stateChangeHandler = { state in
             switch(state) {
             case.favorites(let games):
+                XCTAssertEqual(games.count, 3, "Products count should be 2")
+                XCTAssertEqual(games.first?.name, "Name1", "Name should be Name1")
+                XCTAssertEqual(games[1].name, "Name2", "Name should be Name2")
+                XCTAssertEqual(games[2].name, "Name3", "Name should be Name3")
                 break
             }
         }
     }
 
+    func testFavorites() {
+        let game1 = Game(gameId: 0, name: "Name1")
+        let game2 = Game(gameId: 1, name: "Name2")
+        let game3 = Game(gameId: 2, name: "Name3")
+
+        mockDataSource.addToFavorites(game: game1)
+        mockDataSource.addToFavorites(game: game2)
+        mockDataSource.addToFavorites(game: game3)
+
+        let _ = XCTWaiter.wait(for: [XCTestExpectation(description: "Wait to check change handler")], timeout: 0.1)
+    }
 }
